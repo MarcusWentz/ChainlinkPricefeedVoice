0e12b06f7ae2c2f4af64040a8dc9a6e03597a604
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-03-08 19:33:52 -0500

    Update VoicePricefeedBTC.js

--- VoicePricefeedBTC.js
+++ VoicePricefeedBTC.js
@@ -1,36 +1,42 @@
 //https://gist.github.com/kjantzer/5936491
 /*
- 	Convert numbers to words
+Convert numbers to words
 
- 	copyright 25th July 2006, by Stephen Chapman http://javascript.about.com
- 	permission to use this Javascript on your web page is granted
- 	provided that all of the code (including this copyright notice) is
- 	used exactly as shown (you can change the numbering system if you wish)
+copyright 25th July 2006, by Stephen Chapman http://javascript.about.com
+permission to use this Javascript on your web page is granted
+provided that all of the code (including this copyright notice) is
+used exactly as shown (you can change the numbering system if you wish)
 */
-	var numberToWords = function(s){
-
-		var th = ['','thousand','million', 'billion','trillion'];
-		var dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine'];
-		var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
-		var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-
-		s = (s||'').toString(); s = s.replace(/[\, ]/g,''); if (s != parseFloat(s)) return 'not a number'; var x = s.indexOf('.'); if (x == -1) x = s.length; if (x > 15) return 'too big'; var n = s.split(''); var str = ''; var sk = 0; for (var i=0; i < x; i++) {if ((x-i)%3==2) {if (n[i] == '1') {str += tn[Number(n[i+1])] + ' '; i++; sk=1;} else if (n[i]!=0) {str += tw[n[i]-2] + ' ';sk=1;}} else if (n[i]!=0) {str += dg[n[i]] +' '; if ((x-i)%3==0) str += 'hundred ';sk=1;} if ((x-i)%3==1) {if (sk) str += th[(x-i-1)/3] + ' ';sk=0;}} if (x != s.length) {var y = s.length; str += 'point '; for (var i=x+1; i<y; i++) str += dg[n[i]] +' ';} return str.replace(/\s+/g,' ');
-
-	}
+var numberToWords = function(s){
+	var th = ['','thousand','million', 'billion','trillion'];
+	var dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine'];
+	var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+	var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+	s = (s||'').toString(); s = s.replace(/[\, ]/g,''); if (s != parseFloat(s)) return 'not a number'; var x = s.indexOf('.'); if (x == -1) x = s.length; if (x > 15) return 'too big'; var n = s.split(''); var str = ''; var sk = 0; for (var i=0; i < x; i++) {if ((x-i)%3==2) {if (n[i] == '1') {str += tn[Number(n[i+1])] + ' '; i++; sk=1;} else if (n[i]!=0) {str += tw[n[i]-2] + ' ';sk=1;}} else if (n[i]!=0) {str += dg[n[i]] +' '; if ((x-i)%3==0) str += 'hundred ';sk=1;} if ((x-i)%3==1) {if (sk) str += th[(x-i-1)/3] + ' ';sk=0;}} if (x != s.length) {var y = s.length; str += 'point '; for (var i=x+1; i<y; i++) str += dg[n[i]] +' ';} return str.replace(/\s+/g,' ');
+}
 
 const Web3 = require('web3')
-    const web3ETH = new Web3(process.env.EthereumMainnetInfuraAPIKey);
-    const aggregatorV3InterfaceETHABI = [{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
-    const addrETH = "0xf4030086522a5beea4988f8ca5b36dbc97bee88c";
-    const priceFeedETH = new web3ETH.eth.Contract(aggregatorV3InterfaceETHABI, addrETH);
-    priceFeedETH.methods.latestRoundData().call()
+const web3ETH = new Web3(process.env.EthereumMainnetInfuraAPIKey);
+const addressBTCPricefeedAggregatorV3Interface = "0xf4030086522a5beea4988f8ca5b36dbc97bee88c";
+const aggregatorV3InterfaceABI = [{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+const priceFeedBTC = new web3ETH.eth.Contract(aggregatorV3InterfaceABI, addressBTCPricefeedAggregatorV3Interface);
+var player = require('play-sound')(opts = {})
+
+priceFeedBTC.methods.latestRoundData().call()
         .then((roundData) => {
             let valuePricefeedNumbers = ((roundData.answer)/(10**8)).toFixed(2);
             let valuePricefeedWords = numberToWords(valuePricefeedNumbers).split(" ")
             console.log("BTC_PRICEFEED_MAINNET", valuePricefeedNumbers )
-            console.log("BTC_PRICEFEED_MAINNET")
+            console.log("BTC")
+						player.play('btc.mp3', function(err){if (err) throw err})
             for(let i = 0; i < valuePricefeedWords.length; i++){
               if(valuePricefeedWords[i] !== "")
                 console.log(valuePricefeedWords[i])
+								// try {
+									// console.log(valuePricefeedWords[i] + '.mp3')
+									player.play(valuePricefeedWords[i] + '.mp3', function(err){if (err) throw err})
+								// } catch (e) {
+								// 		console.log(e)
+								// }
               }
         });
