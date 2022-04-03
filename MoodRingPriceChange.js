184e1dea9df5daeea36364ac7ab48816d19205ee
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-04-03 15:44:01 -0400

    Update MoodRingPriceChange.js

--- MoodRingPriceChange.js
+++ MoodRingPriceChange.js
@@ -2,7 +2,7 @@ const Web3 = require('web3')
     const web3ETH = new Web3(process.env.EthereumMainnetInfuraAPIKey);
 
     const aggregatorV3InterfaceETHABI = [{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"description","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint80","name":"_roundId","type":"uint80"}],"name":"getRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"latestRoundData","outputs":[{"internalType":"uint80","name":"roundId","type":"uint80"},{"internalType":"int256","name":"answer","type":"int256"},{"internalType":"uint256","name":"startedAt","type":"uint256"},{"internalType":"uint256","name":"updatedAt","type":"uint256"},{"internalType":"uint80","name":"answeredInRound","type":"uint80"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"version","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
-    const addrETH = "0xf4030086522a5beea4988f8ca5b36dbc97bee88c";
+    const addrETH = "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419";
     const priceFeedETH = new web3ETH.eth.Contract(aggregatorV3InterfaceETHABI, addrETH);
     let valueOld = 0;
     let valueNew = 0;
@@ -47,7 +47,7 @@ async function start(){
       .then((roundData) => {
           // Do something with roundData
           let valueConverted = ((roundData.answer)/(10**8)).toFixed(2)
-          console.log("BTC_PRICE",  valueConverted)
+          console.log("ETH_PRICE",  valueConverted)
           valueNew = valueConverted;
           valueOld = valueNew;
       });
@@ -67,7 +67,7 @@ async function loop(){
         .then((roundData) => {
             // Do something with roundData
             let valueConverted = ((roundData.answer)/(10**8)).toFixed(2)
-            console.log("BTC_PRICE",  valueConverted)
+            console.log("ETH_PRICE",  valueConverted)
             if(i%2){
               valueOld = valueNew;
               valueNew = valueConverted*10;
